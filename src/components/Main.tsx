ad59b2e620c44d05ad8943053c2294ce61def6f7
Ricardo Gonçalves <64687-Ricardocg@users.noreply.drupalcode.org>
2023-11-25 23:20:26 +0000

    small changes (sass, components structure)

---
+++ src/components/Main.tsx
@@ -0,0 +1,15 @@
+
+import Table from "./Table"
+
+
+const Main = () => {
+
+  return (
+    <div className='main_content'>
+      <h1>Movie Ranking</h1>
+      <Table />
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/Table.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+const Table = () => {
+  return (
+    <div className='movies_table'>
+      <div className='movies_table_header'>
+        <p>Ranking</p>
+        <p>Title</p>
+        <p>Year</p>
+        <p>Revenue</p>
+      </div>
+    </div>
+  )
+}
+
+export default Table
