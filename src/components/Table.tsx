9352365d130d59f63b9cc76b65a209cdecb74aa5
Ricardo Gonçalves <64687-Ricardocg@users.noreply.drupalcode.org>
2023-11-27 18:29:03 +0000

    list style

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -29,8 +29,21 @@ const Table = ({data, fetchMoreData, hasMore}: Props) => {
           hasMore={hasMore}
           loader={<p>Loading...</p>}
         >
-          {data.map((item: Movie, index: number) => {
-            return <div className='movie_row_item' key={item.id}>{index} {item.title} {item.id}</div>
+          {data.map((item: Movie) => {
+            const revenue = item.revenue ? Math.trunc(item.revenue * 1000000).toLocaleString() : ''
+            return (
+              <div className='movie_row_item' key={item.id}>
+                <p className='ranking_field'>{item.rank}</p>
+                <p className='title_field'>{item.title}</p>
+                <p className='year_field'>{item.year}</p>
+                <p className='revenue_field'>{revenue && '$'}{revenue}</p>
+                <div className='eye_icon_field'>
+                  <div className='eye_icon_wrapper'>
+                    <img src="src/assets/eye-icon.svg" />
+                  </div>
+                </div>
+              </div>
+            )
           })}
         </InfiniteScroll>
       </div>
