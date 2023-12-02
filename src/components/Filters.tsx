7975f481aaed66cfb9fbf2113f0ecab4d58bb05e
Ricardo Gonçalves <64687-Ricardocg@users.noreply.drupalcode.org>
2023-12-02 13:27:34 +0000

    dark background years dropdown

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -58,7 +58,7 @@ const Filters = ({
           </div>
         </div>
       </div>
-      { (filterOne || filterTwo) ? (
+      {(filterOne || filterTwo) ? (
         <div className='clear_filters' onClick={clearFilters}>
           <img src="src/assets/clear_btn.svg" alt='clear filters button' />
         </div>
